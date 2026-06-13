094a3545180ca91923df7e0f2f4e91325fa91c8a
mnsrulz <manishrawat4u@gmail.com>
2026-06-12 20:31:50 -0400

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { Hono } from "hono";
 import { tasksRouter } from "./endpoints/tasks/router";
 import { ContentfulStatusCode } from "hono/utils/http-status";
 import { streamSSE } from 'hono/streaming';
-import YahooFinance from "https://esm.sh/yahoo-finance2";
+import YahooFinance from "yahoo-finance2";
+
 // Start a Hono app
 const app = new Hono<{ Bindings: Env }>();
 
